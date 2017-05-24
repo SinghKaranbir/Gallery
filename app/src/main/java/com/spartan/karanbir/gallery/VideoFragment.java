c8ee8c0e5294c4bef878e4f542b0f9c4dd0bda3d
SinghKaranbir <karanbir.singh@sjsu.edu>
2017-05-24 01:50:05 -0700

    1. Removing Scalable Video View Library. 2. Adding ExoPlayer. 3. Making Video's to be looping. 4. Using TextureView instead of SimpleExoPlayerView. 5. Transforming TextureView for Scaling.

--- app/src/main/java/com/spartan/karanbir/gallery/VideoFragment.java
+++ app/src/main/java/com/spartan/karanbir/gallery/VideoFragment.java
@@ -1,28 +1,44 @@
 package com.spartan.karanbir.gallery;
 
 
-import android.media.MediaPlayer;
+import android.graphics.Matrix;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.danikula.videocache.HttpProxyCacheServer;
-import com.yqritc.scalablevideoview.ScalableType;
-import com.yqritc.scalablevideoview.ScalableVideoView;
-
-import java.io.IOException;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.LoopingMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 /**
  * @author karanbir.
  * @since 5/20/17.
  */
 
-public class VideoFragment extends Fragment {
+public class VideoFragment extends Fragment implements SimpleExoPlayer.VideoListener {
 
     private String mVideoUrl;
-    private ScalableVideoView scalableVideoView;
+
+    private SimpleExoPlayer mPlayer;
+    private TextureView mTextureView;
+
+    private long playbackPosition;
+    private int currentWindow;
+    private boolean playWhenReady = true;
+
+    private float mWidth, mHeight;
 
     public static VideoFragment newInstance(String videoUrl) {
         VideoFragment videoFragment = new VideoFragment();
@@ -44,24 +60,107 @@ public class VideoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.video_fragment, container, false);
-        scalableVideoView = (ScalableVideoView) view.findViewById(R.id.texture_view);
-        try {
-            HttpProxyCacheServer proxy = GalleryApp.getProxy(getActivity());
-            String proxyUrl = proxy.getProxyUrl(mVideoUrl);
-            scalableVideoView.setDataSource(proxyUrl);
-            scalableVideoView.setScalableType(ScalableType.CENTER_CROP);
-            scalableVideoView.prepareAsync(new MediaPlayer.OnPreparedListener() {
-                @Override
-                public void onPrepared(MediaPlayer mp) {
-                    scalableVideoView.start();
-                    mp.setLooping(true);
-                }
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        mTextureView = (TextureView) view.findViewById(R.id.video_view);
+        mTextureView.post(new Runnable() {
+            @Override
+            public void run() {
+                mWidth = mTextureView.getWidth();
+                mHeight = mTextureView.getHeight();
+            }
+        });
 
         return view;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if ((Util.SDK_INT <= 23 || mPlayer == null)) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
+    }
+
+    private void initializePlayer() {
+        if (mPlayer == null) {
+            mPlayer = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(getActivity()),
+                    new DefaultTrackSelector(), new DefaultLoadControl());
+            mPlayer.setPlayWhenReady(playWhenReady);
+            mPlayer.seekTo(currentWindow, playbackPosition);
+        }
+        mPlayer.setVideoTextureView(mTextureView);
+        MediaSource mediaSource = buildMediaSource(Uri.parse(mVideoUrl));
+        LoopingMediaSource loopingSource = new LoopingMediaSource(mediaSource);
+        mPlayer.prepare(loopingSource, true, false);
+        mPlayer.setVideoListener(this);
+    }
+
+    private void releasePlayer() {
+        if (mPlayer != null) {
+            playbackPosition = mPlayer.getCurrentPosition();
+            currentWindow = mPlayer.getCurrentWindowIndex();
+            playWhenReady = mPlayer.getPlayWhenReady();
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
+    private MediaSource buildMediaSource(Uri uri) {
+        return new ExtractorMediaSource(uri, new DefaultHttpDataSourceFactory("ua"),
+                new DefaultExtractorsFactory(), null, null);
+    }
+
+
+    @Override
+    public void onVideoSizeChanged(int width, int height, int i2, float v) {
+        Matrix matrix = getMatrix(width, height);
+        mTextureView.setTransform(matrix);
+    }
+
+    @Override
+    public void onRenderedFirstFrame() {
+
+    }
+
+
+    private Matrix getMatrix(float videoWidth, float videoHeight) {
+        Matrix matrix = new Matrix();
+
+        float sx = mWidth / videoWidth;
+        float sy = mHeight / videoHeight;
+        float maxScale = Math.max(sx, sy);
+        sx = maxScale / sx;
+        sy = maxScale / sy;
+        float px = mWidth / 2;
+        float py = mHeight / 2;
+
+        matrix.setScale(sx, sy, px, py);
+        return matrix;
+
+    }
+
+
 }
